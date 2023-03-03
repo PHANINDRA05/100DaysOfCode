8f5195f6f9bccce749bb1124b5e83a1a81a9b2ad
pkakarap <kakaraparthiphani0512@gmail.com>
2023-03-03 14:19:51 +0530

    Day 10

---
+++ Day10/calculator.py
@@ -0,0 +1,52 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(num1, num2):
+    return num1 + num2
+def sub(num1, num2):
+    return num1 - num2
+def mul(num1, num2):
+    return num1 * num2
+def div(num1, num2):
+    return num1 / num2
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+    operations = {
+        '+' : add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
+    for operator in operations:
+        print(operator)
+
+    while True:
+        operator = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calling_func = operations[operator]
+        ans = calling_func(num1,num2)
+        print(f"{num1} {operator} {num2} = {ans}")
+        restart = input("Type 'y' to continue calculating with 11.0, or type 'n' to start a new calculation: ").lower()
+        if restart == 'y':
+            num1 = ans
+        else:
+            calculator()
+
+calculator()
